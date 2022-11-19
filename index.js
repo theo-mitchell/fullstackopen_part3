2c97661db46acc9cbfce244696eeeab37b6998d6
theom95 <theodore.mitchell95@gmail.com>
2022-11-18 18:49:54 -0600

    step 3.2 done

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
 ];
 
-app.get('/api/persons', (req, res) => {
-    return res.json(persons);
-})
+app.get("/info", (req, res) => {
+  let message = `Phonebook has info for ${persons.length} people </br>`;
+  let currentTime = new Date();
+  res.send(message + currentTime);
+});
+
+app.get("/api/persons", (req, res) => {
+  return res.json(persons);
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
+  console.log(`Server running on port ${PORT}`);
+});
